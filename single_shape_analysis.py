dc2f01862663465c1bcfe31df063599b794f7da5
tingyang <jtyjsbzq@gmail.com>
2023-10-16 16:37:49 +0200

    add surface area

--- single_shape_analysis.py
+++ single_shape_analysis.py
@@ -14,8 +14,9 @@ def single_shape_analysis(mesh):
 
 	num_vertices = np.asarray(mesh.vertices).shape[0]
 	num_triangles = np.asarray(mesh.triangles).shape[0]
+	surface_area = mesh.get_surface_area()
 
-	return num_vertices, num_triangles
+	return num_vertices, num_triangles, surface_area
 
 
 
@@ -24,11 +25,13 @@ def single_shape_analysis(mesh):
 """
 file = 'ShapeDatabase_INFOMR-master/Car/D00168.obj'
 mesh = read_mesh(file)
-num_vertices, num_triangles = single_shape_analysis(mesh)
+num_vertices, num_triangles, surface_area = single_shape_analysis(mesh)
 #print(mesh)
 print("The amount of vertices in this shape is", num_vertices)
 print("The amount of triangles in this shape is", num_triangles)
 print("The class of this shape is", file.split("/")[1])
+print("The surface area is ", surface_area)
 visualize_mesh(mesh, vis_option = "wireframe_on_shaded")
 """
+
 
